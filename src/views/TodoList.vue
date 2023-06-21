88cc4c9effa4bd04775959e9eaa0b856e3c9a51c
moonlightboss <cumrak.legenda@gmail.com>
2023-06-21 12:40:58 +0300

    Remove old component

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -70,20 +70,12 @@ onMounted(() => {
       </form>
     </section>
     <section class="todo-list">
-      <h3>TODO LIST</h3>
+      <h3>TASK LIST</h3>
       <div class="list" id="todo-list">
         <div
           v-for="todo in todos_asc"
           :class="`todo-item ${todo.done && 'done'}`"
         >
-          <label>
-            <input type="checkbox" v-model="todo.done" />
-            <span
-              :class="`bubble ${
-                todo.category == 'business' ? 'business' : 'personal'
-              }`"
-            ></span>
-          </label>
 
           <div class="todo-content">
             <input type="text" v-model="todo.content" />
